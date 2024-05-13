0f9eaf05df83248659657b9efb14adbd2cc138b0
zixeel <strpan549@gmail.com>
2024-05-13 03:59:20 +0300

    add CourseDetailSerializer

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,3 +1,4 @@
+from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 
 from materials.models import Course, Lesson
@@ -8,6 +9,16 @@ class CourseSerializer(ModelSerializer):
         model = Course
         fields = "__all__"
 
+
+class CourseDetailSerializer:
+    lesson_in_course = SerializerMethodField()
+
+    def get_lesson_in_course(self, lesson):
+        return Lesson.objects.filter(course=lesson.course)
+
+    class Meta:
+        model = Course
+        fields = ("name", "description",)
 
 
 class LessonSerializer(ModelSerializer):
