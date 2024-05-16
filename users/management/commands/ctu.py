05e5af22f6ee9c56ca1475f3d83b455d76eba89f
zixeel <strpan549@gmail.com>
2024-05-16 08:41:11 +0300

    expanded custon comand

--- users/management/commands/ctu.py
+++ users/management/commands/ctu.py
@@ -1,13 +1,14 @@
 from django.core.management import BaseCommand
 
+from materials.models import Course
 from users.models import User, Payment
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         user = User.objects.create(
-            email='admin@mail.ru',
-            phone='111',
+            email='admin4@mail.ru',
+            phone='1112',
             country='Кадия',
             is_staff=True,
             is_superuser=True
@@ -15,5 +16,17 @@ class Command(BaseCommand):
         user.set_password('5682')
         user.save()
 
+        course = Course.objects.create(
+            name='drf-3',
+            description='some text'
+        )
+        course.save()
 
-
+        paid_course = course
+        payment = Payment.objects.create(
+            user=user,
+            paid_course=paid_course,
+            payment_amount=5500,
+            payment_method='CASHLESS'
+        )
+        payment.save()
